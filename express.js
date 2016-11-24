fe190e3fea42b607de7c413e9de764b1566eb350
oblivionxx <Zhengni@Zhengnis-MacBook-Air-2.local>
2016-11-24 22:07:57 +0000

    Dynamic URL

--- express.js
+++ express.js
@@ -13,4 +13,10 @@ app.get('/basement', function(req, res) {
     res.end('You\’re in the wine cellar. Those bottles are mine!');
 });
 
+//dynamic url
+app.get('/floor/:floornum/bedroom', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('You\’re in the bedroom on floor n°' + req.params.floornum);
+});
+
 app.listen(8080);
